70569185bb607bfd95e427062a1d54c23e1dc351
thanadon lamsan <thanadon11406@gmail.com>
2024-01-27 15:54:25 +0700

    Initial commit

---
+++ src/test/java/thanadon/project/shoppingMall/ShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package thanadon.project.shoppingMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
